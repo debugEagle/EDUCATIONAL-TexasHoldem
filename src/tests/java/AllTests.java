d88a0c2ae9449ddaf07ce9aa837ad992a860ab72
Jonathon Davis <jonathondevindavis@gmail.com>
2016-08-27 12:04:08 -0700

    Added allTests junit suit

---
+++ src/tests/java/AllTests.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package tests.java;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author metro
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ PokerHandTest.class, RankSuitAndCardTest.class })
+public class AllTests {
+
+}
